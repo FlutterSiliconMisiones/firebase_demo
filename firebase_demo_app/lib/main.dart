9041494d2d7b7bb79870c2bfaade6a486d902d20
Martin Peruchena <tinoper@gmail.com>
2023-09-10 21:24:37 -0300

    Add Dummy Data

--- firebase_demo_app/lib/main.dart
+++ firebase_demo_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_demo_app/services/database/database_service.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -8,16 +9,36 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return AppView();
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () async {
+                DatabaseService databaseService = DatabaseService();
+                await databaseService.saveDummyData();
+              },
+              icon: Icon(Icons.data_saver_on),
+            ),
+          ],
+        ),
         body: Center(
           child: Text('Hello World!'),
         ),
---
+++ firebase_demo_app/lib/services/database/database_service.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<void> saveDummyData() async {
+    final productos = [
+      {
+        'nombre': 'Camiseta',
+        'precio': 20.0,
+      },
+      {
+        'nombre': 'Pantalones',
+        'precio': 30.0,
+      },
+      {
+        'nombre': 'Zapatos',
+        'precio': 50.0,
+      },
+      {
+        'nombre': 'Gorra',
+        'precio': 10.0,
+      },
+      {
+        'nombre': 'Calcetines',
+        'precio': 5.0,
+      },
+      {
+        'nombre': 'Chaqueta',
+        'precio': 60.0,
+      },
+      {
+        'nombre': 'Bufanda',
+        'precio': 15.0,
+      },
+      {
+        'nombre': 'Bolsa',
+        'precio': 25.0,
+      },
+      {
+        'nombre': 'Reloj',
+        'precio': 70.0,
+      },
+      {
+        'nombre': 'Gafas de sol',
+        'precio': 40.0,
+      },
+    ];
+
+    for (var producto in productos) {
+      await _db.collection('productos').add(producto);
+    }
+  }
+}
